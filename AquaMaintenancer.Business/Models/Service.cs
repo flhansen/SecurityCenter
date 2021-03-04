bd25551397d85034d16ccf8a06a1e3e2f12892fa
Florian Hansen <flhansen.93@gmail.com>
2021-03-04 15:55:15 +0100

    Initial commit. Add service wrapper class in business logic.

---
+++ AquaMaintenancer.Business/Models/Service.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace AquaMaintenancer.Business.Models
+{
+    public class Service
+    {
+        public ServiceController Controller { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string ServiceName { get; set; }
+
+        public ServiceStartMode StartMode { get; set; }
+
+        public ServiceControllerStatus Status { get; set; }
+    }
+}
