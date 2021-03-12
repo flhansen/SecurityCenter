397b489d48009d1abd7797f01eea5ee655f455c4
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-03-12 18:48:17 +0100

    Add windowsupdate model.

---
+++ AquaMaintenancer.Business/Models/WindowsUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WUApiLib;
+
+namespace AquaMaintenancer.Business.Models
+{
+    public class WindowsUpdate
+    {
+        public bool IsOfflineUpdate { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string KbNumber { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        [JsonIgnore]
+        public IUpdate Update { get; set; }
+    }
+}
