96757aab02cac8f0d0ba2b15143282dbbcf52569
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-03-12 16:59:55 +0100

    Add antivirus model.

---
+++ AquaMaintenancer.Business/Models/AntiVirus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaMaintenancer.Business.Models
+{
+    public class AntiVirus
+    {
+        public string InstanceGuid { get; set; }
+        public string DisplayName { get; set; }
+        public string PathToSignedProductExe { get; set; }
+        public string PathToSignedReportingExe { get; set; }
+        public uint ProductState { get; set; }
+        public string Timestamp { get; set; }
+    }
+}
