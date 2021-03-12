5f9b3db4ece5207b11aef93d90ead2ce3efaada7
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-03-12 18:39:14 +0100

    Add windowsevent model.

---
+++ AquaMaintenancer.Business/Models/WindowsEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaMaintenancer.Business.Models
+{
+    public class WindowsEvent
+    {
+        public DateTime TimeGenerated { get; set; }
+        public EventLogEntryType EntryType { get; set; }
+        public long InstanceId { get; set; }
+        public string Message { get; set; }
+        public string Source { get; set; }
+        public int Index { get; set; }
+    }
+}
