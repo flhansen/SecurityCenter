c34c8ed3d314f33cf8d41a6dbe98c67dfe28cd3b
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-03-12 18:37:40 +0100

    Add systeminformation model.

---
+++ AquaMaintenancer.Business/Models/SystemInformation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaMaintenancer.Business.Models
+{
+    public class SystemInformation
+    {
+        public string ComputerName { get; set; }
+        public string UserName { get; set; }
+        public string OperatingSystem { get; set; }
+        public string SystemType { get; set; }
+        public string Manufacturer { get; set; }
+        public string Architecture { get; set; }
+        public ulong BytesRam { get; set; }
+    }
+}
