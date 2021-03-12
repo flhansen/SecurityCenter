81406fb006322630796e99bea2ad9bb00814ad52
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-03-12 18:40:39 +0100

    Add windowsevent collection.

---
+++ AquaMaintenancer.Business/Models/WindowsEventCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaMaintenancer.Business.Models
+{
+    public class WindowsEventCollection : ObservableCollection<WindowsEvent>
+    {
+        public string LogDisplayName { get; set; }
+        public string LogName { get; set; }
+    }
+}
