dc66099367ca7a0b397578ec8d7a06779a250238
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-03-12 17:09:22 +0100

    Add loadingprogress model.

---
+++ AquaMaintenancer.Business/Models/LoadingProgress.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaMaintenancer.Business.Models
+{
+    public class LoadingProgress
+    {
+        public Dictionary<Action, string> Actions { get; set; } = new Dictionary<Action, string>();
+    }
+}
