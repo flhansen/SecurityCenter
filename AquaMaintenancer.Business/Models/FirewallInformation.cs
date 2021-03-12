bcea2a373d9014c0288a07e5e0796a2dff8f0fcd
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-03-12 17:07:45 +0100

    Add firewallinformation model.

---
+++ AquaMaintenancer.Business/Models/FirewallInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaMaintenancer.Business.Models
+{
+    public class FirewallInformation
+    {
+        public bool IsPrivateActivated { get; set; }
+        public bool IsPublicActivated { get; set; }
+        public bool IsDomainActivated { get; set; }
+    }
+}
