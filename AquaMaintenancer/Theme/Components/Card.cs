454fe6c86fccaeabd95a9c1e45ed09e413348679
Kluddizz <flhansen.93@gmail.com>
2021-03-16 23:31:20 +0100

    Add color palette to ressource dictionary and implement basic card component. I know, it's bad practice, because implementing components needs to be done in separate branches, but I don't want to reimplement the whole thing.

---
+++ AquaMaintenancer/Theme/Components/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AquaMaintenancer.Theme.Components
+{
+    public class Card : ContentControl
+    {
+        static Card()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Card), new FrameworkPropertyMetadata(typeof(Card)));
+        }
+
+        public string Title
+        {
+            get => GetValue(TitleProperty) as string;
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string),
+                typeof(Card), new PropertyMetadata(string.Empty));
+    }
+}
