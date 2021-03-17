59c676398dbe18652840a87ee78c706fef7cc61b
Kluddizz <flhansen.93@gmail.com>
2021-03-17 15:23:46 +0100

    Implement label of textfields. Create converter class to render the label content in uppercase.

--- AquaMaintenancer/Theme/Components/TextField.cs
+++ AquaMaintenancer/Theme/Components/TextField.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace AquaMaintenancer.Theme.Components
 {
     public class TextField : TextBox
     {
+        public string Label
+        {
+            get => GetValue(LabelProperty) as string;
+            set => SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(nameof(Label), typeof(string),
+                typeof(TextField), new PropertyMetadata(string.Empty));
     }
 }
