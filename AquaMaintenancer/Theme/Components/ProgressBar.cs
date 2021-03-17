67cb074700799fc244cb320ef78d7a5b18b73e7f
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-03-17 22:38:35 +0100

    Add ProgressBar.cs to Components.

---
+++ AquaMaintenancer/Theme/Components/ProgressBar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls.Primitives;
+
+namespace AquaMaintenancer.Theme.Components
+{
+    class ProgressBar : RangeBase
+    {
+
+    }
+}
