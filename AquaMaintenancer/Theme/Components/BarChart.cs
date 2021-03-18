a8e8856416c0ce2649f715312da493ecdfdc509f
Kluddizz <flhansen.93@gmail.com>
2021-03-18 20:58:28 +0100

    Add bar chart component.

---
+++ AquaMaintenancer/Theme/Components/BarChart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AquaMaintenancer.Theme.Components
+{
+    public class ChartData
+    {
+        public object Category { get; set; }
+        public object Value { get; set; }
+    }
+
+    public class BarChart : Control
+    {
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register(nameof(Data), typeof(IEnumerable<ChartData>),
+                typeof(BarChart), new PropertyMetadata());
+
+        public IEnumerable<ChartData> Data
+        {
+            get => (IEnumerable<ChartData>) GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+    }
+}
