1bc3c1edf027d81bae93eaf0a0fe7a413cfef120
Florian Hansen <flhansen.93@gmail.com>
2021-03-18 15:01:34 +0100

    Implement styles for button and icon button. Change font etc.

--- AquaMaintenancer/Theme/Components/PrimaryButton.cs
+++ AquaMaintenancer/Theme/Components/PrimaryButton.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using FontAwesome.WPF;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace AquaMaintenancer.Theme.Components
 {
     public class PrimaryButton : Button
     {
+        #region Icon Property
+        public static DependencyProperty IconProperty = 
+            DependencyProperty.Register(nameof(Icon), typeof(FontAwesomeIcon),
+                typeof(PrimaryButton), new PropertyMetadata());
+
+        public FontAwesomeIcon Icon
+        {
+            get => (FontAwesomeIcon)GetValue(IconProperty);
+            set => SetValue(IconProperty, value); 
+        }
+        #endregion
     }
 }
