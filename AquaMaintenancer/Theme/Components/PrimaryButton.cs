05d2d60f4d19abd4b952a7d9aaf4b59765f6c65d
Kluddizz <flhansen.93@gmail.com>
2021-03-25 15:26:56 +0100

    Add hover effects to PrimaryButton.

--- AquaMaintenancer/Theme/Components/PrimaryButton.cs
+++ AquaMaintenancer/Theme/Components/PrimaryButton.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace AquaMaintenancer.Theme.Components
 {
@@ -18,8 +19,22 @@ namespace AquaMaintenancer.Theme.Components
             set => SetValue(IconProperty, value); 
         }
 
-        public static DependencyProperty IconProperty = 
+        public static readonly DependencyProperty IconProperty = 
             DependencyProperty.Register(nameof(Icon), typeof(FontAwesomeIcon),
                 typeof(PrimaryButton), new PropertyMetadata());
+
+        /// <summary>
+        /// Describes how much the background color will be darkened on mouse over.
+        /// The value must be between -1.0 and 1.0.
+        /// </summary>
+        public double BrightnessValue
+        {
+            get => (double)GetValue(BrightnessValueProperty);
+            set => SetValue(BrightnessValueProperty, value); 
+        }
+
+        public static readonly DependencyProperty BrightnessValueProperty = 
+            DependencyProperty.Register(nameof(BrightnessValue), typeof(double),
+                typeof(PrimaryButton), new PropertyMetadata(-1.0));
     }
 }
