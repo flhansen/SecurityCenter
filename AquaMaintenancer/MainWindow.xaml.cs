5783078cce0b1db068a760ff371f3b2aab7b26fd
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-03-28 00:31:27 +0100

    Add a not functional version of progressbar

--- AquaMaintenancer/MainWindow.xaml.cs
+++ AquaMaintenancer/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,19 @@ namespace AquaMaintenancer
         public MainWindow()
         {
             InitializeComponent();
+
+            float val = 0f;
+
+            new Thread(() =>
+            {
+                while(val <= 100)
+                {
+                    Thread.Sleep(500);
+                    val += 5;
+                    Dispatcher.Invoke(new Action(() => pgb.Value = val));
+                }
+            }).Start();
+
         }
     }
 }
--- AquaMaintenancer/Theme/Components/ProgressBar.cs
+++ AquaMaintenancer/Theme/Components/ProgressBar.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 
 namespace AquaMaintenancer.Theme.Components
 {
-    class ProgressBar : RangeBase
+    public class ProgressBar : ContentControl
     {
+        private static Border indicator;
+        static ProgressBar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ProgressBar), new FrameworkPropertyMetadata(typeof(ProgressBar)));
+        }
+        public float Value 
+        { 
+            get => (float)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, Value);
+        }
 
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value), typeof(float), typeof(ProgressBar), 
+            new PropertyMetadata(float.NaN, HandlePropertyChanged));
+        
+        private static void HandlePropertyChanged(object d, DependencyPropertyChangedEventArgs e)
+        {
+            indicator.Width = (double)e.NewValue;
+            indicator.InvalidateVisual();
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            indicator = GetTemplateChild("Indicator") as Border;
+
+            if (indicator == null)
+            {
+                throw new NullReferenceException("Templatepart not found");
+            }
+        }
     }
 }
