58a5cf1259b802118dc32ba355337f1467b19e90
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-03-29 13:35:07 +0200

    Add barebone of UserCompoenent and the UserCompoenent.cs file.

---
+++ AquaMaintenancer/Theme/Components/UserControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaMaintenancer.Theme.Components
+{
+    public class UserControlData
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string PicturePath { get; set; }
+    }
+    public class UserControl
+    {
+    }
+}
