a365ea15350c5ed95c912f90d4b67bd698d420cb
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-04-07 23:26:20 +0200

    Add MenuItem

---
+++ AquaMaintenancer/Theme/Components/MenuItem.cs
@@ -0,0 +1,26 @@
+﻿using FontAwesome.WPF;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AquaMaintenancer.Theme.Components
+{
+    public class MenuItem : ContentControl
+    { 
+        /// <summary>
+        /// The icon inside the button. If <em>None</em>, it will not be displayed.
+        /// </summary>
+        public FontAwesomeIcon Icon
+        {
+            get => (FontAwesomeIcon)GetValue(IconProperty);
+            set => SetValue(IconProperty, value); 
+        }
+
+        public static readonly DependencyProperty IconProperty = 
+            DependencyProperty.Register(nameof(Icon), typeof(FontAwesomeIcon),
+                typeof(MenuItem), new PropertyMetadata());
+
+    }
+}
