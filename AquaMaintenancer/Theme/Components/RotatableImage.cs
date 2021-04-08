0c0e9d73882eafea7adec6e1daf277dbf0e60863
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-04-08 17:02:32 +0200

    Seperate rotating icon

---
+++ AquaMaintenancer/Theme/Components/RotatableImage.cs
@@ -0,0 +1,66 @@
+﻿using FontAwesome.WPF;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace AquaMaintenancer.Theme.Components
+{
+    public class RotatableImage : ImageAwesome
+    {
+        private DoubleAnimation RotateForth;
+        private DoubleAnimation RotateBack;
+        private Storyboard Sb;
+        private RotationStatus RotationDirection = RotationStatus.Forth;
+
+        public static readonly RoutedEvent OnClickRotateEvent = EventManager.RegisterRoutedEvent(
+            "OnClickRotate", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(RotatableImage));
+
+        public event RoutedEventHandler OnClickRotate
+        {
+            add => AddHandler(OnClickRotateEvent, value);
+            remove => RemoveHandler(OnClickRotateEvent, value);
+        }
+
+        void RaiseRotateEvent()
+        {
+            RoutedEventArgs newEventArgs = new RoutedEventArgs(OnClickRotateEvent);
+            RaiseEvent(newEventArgs);
+
+            Sb.Stop();
+
+            if (RotationDirection == RotationStatus.Forth)
+            {
+                Sb.Children.Add(RotateForth);
+                RotationDirection = RotationStatus.Back;
+            }
+            else
+            {
+                Sb.Children.Add(RotateBack);
+                RotationDirection = RotationStatus.Forth;
+            }
+
+            Sb.Begin(); 
+        }
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            RaiseRotateEvent();
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            RotateBack = GetTemplateChild("RotateBack") as DoubleAnimation;
+            RotateForth = GetTemplateChild("RotateForth") as DoubleAnimation;
+            Sb = GetTemplateChild("Carot") as Storyboard;
+
+            if (RotateForth == null || RotateForth == null || Sb == null)
+            {
+                throw new NullReferenceException("Animations not found.");
+            }
+        }
+    }
+}
--- AquaMaintenancer/Theme/Components/UserComponent.cs
+++ AquaMaintenancer/Theme/Components/UserComponent.cs
@@ -19,57 +19,5 @@ namespace AquaMaintenancer.Theme.Components
     }
     public class UserComponent : ContentControl
     {
-        private DoubleAnimation RotateForth;
-        private DoubleAnimation RotateBack;
-        private Storyboard Sb;
-        private RotationStatus RotationDirection = RotationStatus.Forth;
-
-        public static readonly RoutedEvent OnClickRotateEvent = EventManager.RegisterRoutedEvent(
-            "OnClickRotate", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(UserComponent));
-
-        public event RoutedEventHandler OnClickRotate
-        {
-            add => AddHandler(OnClickRotateEvent, value);
-            remove => RemoveHandler(OnClickRotateEvent, value);
-        }
-
-        void RaiseRotateEvent()
-        {
-            RoutedEventArgs newEventArgs = new RoutedEventArgs(OnClickRotateEvent);
-            RaiseEvent(newEventArgs);
-
-            Sb.Stop();
-
-            if (RotationDirection == RotationStatus.Forth)
-            {
-                Sb.Children.Add(RotateForth);
-                RotationDirection = RotationStatus.Back;
-            }
-            else
-            {
-                Sb.Children.Add(RotateBack);
-                RotationDirection = RotationStatus.Forth;
-            }
-
-            Sb.Begin(); 
-        }
-        protected override void OnMouseDown(MouseButtonEventArgs e)
-        {
-            base.OnMouseDown(e);
-            RaiseRotateEvent();
-        }
-
-        public override void OnApplyTemplate()
-        {
-            base.OnApplyTemplate();
-            RotateBack = GetTemplateChild("RotateBack") as DoubleAnimation;
-            RotateForth = GetTemplateChild("RotateForth") as DoubleAnimation;
-            Sb = GetTemplateChild("Carot") as Storyboard;
-
-            if (RotateForth == null || RotateForth == null || Sb == null)
-            {
-                throw new NullReferenceException("Animations not found.");
-            }
-        }
     }
 }
