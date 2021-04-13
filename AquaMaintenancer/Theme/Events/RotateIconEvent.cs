6a31754fac465ba1652ceb2ba2ee16673254628c
mabe5347 <markus.behnisch@stud.hs-flensburg.de>
2021-04-14 00:51:07 +0200

    Try to encapsulate the customevent

---
+++ AquaMaintenancer/Theme/Events/RotateIconEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace AquaMaintenancer.Theme.Events
+{
+    class RotateIconEvent
+    {
+        public event RoutedEventHandler Test;
+
+         public static readonly RoutedEvent OnClickRotateEvent = EventManager.RegisterRoutedEvent(
+            "OnClickRotate", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(RotateIconEvent));
+
+        public event RoutedEventHandler OnClickRotate
+        {
+            add => AddHandler(OnClickRotateEvent, value);
+            remove => RemoveHandler(OnClickRotateEvent, value);
+        }
+
+    }
+}
