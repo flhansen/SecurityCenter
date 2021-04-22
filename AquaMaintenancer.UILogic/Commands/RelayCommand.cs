dfa3be92a38de0bb11d5c852d8da3bf77bf10f36
Florian Hansen <flhansen.93@gmail.com>
2021-04-22 03:33:06 +0200

    Implement side menu. Selected menu items will result in changing the bound page ViewModel. Based on the bound page ViewModel, the corresponding item will be highlighted.

---
+++ AquaMaintenancer.UILogic/Commands/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace AquaMaintenancer.UILogic.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> action;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> action, Func<object, bool> canExecute)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object> action) : this(action, null)
+        {
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute != null)
+            {
+                return canExecute.Invoke(parameter);
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            action.Invoke(parameter);
+        }
+    }
+}
---
+++ AquaMaintenancer/Theme/Converter/PageViewModelToBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿using AquaMaintenancer.UILogic.ViewModels.Core;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace AquaMaintenancer.Theme.Converter
+{
+    public class PageViewModelToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Type targetViewModelType = parameter as Type;
+            ViewModelBase currentViewModel = value as ViewModelBase;
+
+            return currentViewModel.GetType().Equals(targetViewModelType);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
