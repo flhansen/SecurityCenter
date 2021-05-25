74a7251a9ef525a9d115bb5271db3a4001afd880
Florian Hansen <flhansen.93@gmail.com>
2021-05-25 16:23:29 +0200

    Add business logic for port scans.

---
+++ SecurityCenter.Business/Models/PortScan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityCenter.Business.Models
+{
+    public class PortScan
+    {
+        public IPAddress Destination { get; set; }
+        public IEnumerable<int> Ports { get; set; }
+        public bool ScanIndividualPorts { get; set; }
+        public bool IsAggressive { get; set; }
+        public bool ScanRunningServices { get; set; }
+        public bool InterpretOSFingerprint { get; set; }
+        public bool SaveResultsInFile { get; set; }
+        public string SaveFileName { get; set; }
+    }
+}
